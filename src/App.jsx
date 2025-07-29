a57bf75f5b4460b9a1072a4dd0cf1cb4cbb9bbc8
paulseeland <paul.seeland@gmail.com>
2025-07-29 20:24:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
           <PlayerForm 
             players={players}
             addPlayer={addPlayer}
-            satrtGame={startGame}
+            startGame={startGame}
           />
         )}
 
